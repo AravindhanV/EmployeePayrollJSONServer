53dbbe8b19ab039ef2feb5e3d3a35e5771f6c4e6
Aravindhan V <varavindhan2010@gmail.com>
2021-10-11 22:37:34 +0530

    ADDED: method to get form data while submitting

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -3,3 +3,12 @@ var salary = document.querySelector("#salary");
 salary.addEventListener("input", function () {
   salaryOutput.textContent = salary.value;
 });
+
+function save(e) {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let date = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+  let startDate = Date.parse(year + "-" + month + "-" + date);
+}
