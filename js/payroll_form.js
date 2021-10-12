52d141090b2c692e131acef3d04c934d80aec867
Aravindhan V <varavindhan2010@gmail.com>
2021-10-12 20:43:38 +0530

    ADDED: function to reset form to default value on clicking reset button

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -68,6 +68,30 @@ const createAndUpdateStorage = (employeePayrollData) => {
   );
 };
 
+const resetForm = () => {
+  setValue("#name", "");
+  unsetSelectedValues("[name=profile]");
+  unsetSelectedValues("[name=gender");
+  unsetSelectedValues("[name=department");
+  setValue("#salary", "");
+  setValue("#notes", "");
+  setValue("#day", "1");
+  setValue("#month", "1");
+  setValue("#year", "2021");
+};
+
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach((item) => {
+    item.checked = false;
+  });
+};
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+};
+
 const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
   try {
