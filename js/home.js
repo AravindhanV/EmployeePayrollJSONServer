5b35db5e2063447bb1630d0b42cd4e8698e1c33f
Aravindhan V <varavindhan2010@gmail.com>
2021-10-13 22:40:23 +0530

    MODIFIED: home page to load payroll data from LocalStorage

--- js/home.js
+++ js/home.js
@@ -1,22 +1,34 @@
+let employeePayrollList;
 window.addEventListener("DOMContentLoaded", (event) => {
+  employeePayrollList = getEmployeePayrollDataFromStorage();
+  document.querySelector(".emp-count").textContent = employeePayrollList.length;
   createInnerHtml();
+  localStorage.removeItem("editEmp");
 });
+
+const getEmployeePayrollDataFromStorage = () => {
+  return localStorage.getItem("EmployeePayrollList")
+    ? JSON.parse(localStorage.getItem("EmployeePayrollList"))
+    : [];
+};
 
 const createInnerHtml = () => {
   const headerHtml =
     "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th>" +
     "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+  if (employeePayrollList.length == 0) return;
   let innerHtml = `${headerHtml}`;
-  let empPayrollList = createEmployeePayrollJSON();
-  for (const empPayrollData of empPayrollList) {
+  for (const empPayrollData of employeePayrollList) {
     innerHtml = `${innerHtml}
         <tr>
-        <td><img class="profile" alt="" src="${empPayrollData._picture}"></td>
+        <td><img class="profile" alt="" src="${
+          empPayrollData._profilePic
+        }"></td>
         <td>${empPayrollData._name}</td>
         <td>${empPayrollData._gender}</td>
         <td>${getDeptHtml(empPayrollData._department)}
         <td>${empPayrollData._salary}</td>
-        <td>${empPayrollData._startDate}</td>
+        <td>${formatDate(empPayrollData._startDate)}</td>
         <td>
         <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
         <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
@@ -27,31 +39,11 @@ const createInnerHtml = () => {
   document.querySelector("#table-display").innerHTML = innerHtml;
 };
 
-const createEmployeePayrollJSON = () => {
-  let employeePayrollListLocal = [
-    {
-      _name: "Narayan Mahadevan",
-      _gender: "Male",
-      _department: ["Finance", "Engineering"],
-      _salary: "5000000",
-      _startDate: "29 Oct, 2020",
-      _note: "",
-      _id: new Date().getTime(),
-      _picture: "../assets/profile-images/Ellipse -2.png",
-    },
-    {
-      _name: "ABC",
-      _gender: "Female",
-      _department: ["Sales", "Engineering"],
-      _salary: "750000",
-      _startDate: "10 Dec 2019",
-      _note: "",
-      _id: new Date().getTime() + 1,
-      _picture: "../assets/profile-images/Ellipse -7.png",
-    },
-  ];
-
-  return employeePayrollListLocal;
+const formatDate = (date) => {
+  date = new Date(date);
+  return (
+    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
+  );
 };
 
 const getDeptHtml = (departmentList) => {
