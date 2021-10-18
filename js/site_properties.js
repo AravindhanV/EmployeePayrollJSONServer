5d65d2e4103946bdda6849a92156f53d40df3081
Aravindhan V <varavindhan2010@gmail.com>
2021-10-18 22:05:58 +0530

    MODIFIED: save method in payroll_form to support server POST requests

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,6 +1,6 @@
 let site_properties = {
   home_page: "../pages/home.html",
   add_emp_payroll_page: "../pages/payroll_form.html",
-  use_local_storage: "true",
-  server_url: "htpp://127.0.0.1:3000/EmployeePayrollDB",
+  use_local_storage: "false",
+  server_url: "http://127.0.0.1:3000/EmployeePayrollDB",
 };
